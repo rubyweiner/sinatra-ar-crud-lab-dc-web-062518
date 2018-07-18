6a1682bb1a90ce56f26a693f44522b6fc9bd0e2d
Ruby Weiner <rubyweiner796@me.com>
2018-07-18 15:19:42 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,18 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+
+  # attr_accessor :attributes
+  #
+  # @@all = []
+  #
+  # def initialize(attributes)
+  #   @name = attributes[:name]
+  #   @content = attributes[:content]
+  #   @@all << self
+  # end
+  #
+  # def self.all
+  #   @@all
+  # end
 end
